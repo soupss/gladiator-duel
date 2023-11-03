cfff81634a7355ba52d081b1300788d7e818debd
Anton Laurell <antonlaurell@hotmail.com>
2023-11-03 01:04:28 +0100

    doesnt work

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 import pygame
 
 
+def init():
+    pygame.init()
+    window_width = 640
+    window_height = 480
+    window_size = (window_width, window_height)
+    screen = pygame.display.set_mode(window_size)
+    pygame.display.set_caption('Pygame Window')
+
+
+def update():
+    screen.fill((0, 120, 255))
+    pygame.display.flip()
+
+
+def events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+def close():
+    pygame.quit()
+    sys.exit()
+
+
+while(1):
+    init()
+    update()
+    events()
+    close()
