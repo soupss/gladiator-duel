92b5fdc71aab72c2c3db0d10504c5d751a266619
Anton Laurell <antonlaurell@hotmail.com>
2023-11-03 01:26:07 +0100

    blue screen

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import pygame
+from Game import Game
 
 
 def main():
-    game = Game
-    while(game.isRunning):
+    game = Game()
+    while game.isRunning():
         game.events()
         game.update()
     game.close()
+
+if __name__ == "__main__":
+    main()
